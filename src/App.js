4b8d8c4cebc50064a489712ca9e78356dcb2914f
Mike <mikemolner@gmail.com>
2019-09-11 16:04:58 -0700

    intitial commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <h1>Account Overview</h1>
+    <br></br>
+
+    <h5>Profile</h5>
+    <br></br>
+
+    <div class="container">
+        <form>
+            <div class="form-group row">
+                <label for="input1">Username</label>
+                <input type="text" class="form-control" id="input1" placeholder="Username">
+            </div>
+            <div class="form-group row">
+                <label for="input2">Email</label>
+                <input type="text" class="form-control" id="input2" placeholder="Email">
+            </div>
+            <div class="form-group row">
+                <label for="input2">Date of birth</label>
+                <input type="text" class="form-control" id="input2" placeholder="Date of birth">
+            </div>
+            <div class="form-group row">
+                <label for="input2">Country</label>
+                <input type="text" class="form-control" id="input2" placeholder="Country">
+            </div>
+        </form>
+    </div>
+
+    <br></br>
+
+    <button class="btn btn-outline-dark"style="background-color: #d4c81b;">Edit Profile</button>  <button class="btn btn-outline-dark"style="background-color: #d4c81b;">My Plan</button> <button class="btn btn-outline-dark"style="background-color: #d4c81b;">My Account</button> 
+    
+    
     </div>
   );
 }
