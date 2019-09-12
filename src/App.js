e703af8e1724d1b77f13943fdd215b5183070bd6
Anthony Martinez <Martinezanthony213@gmail.com>
2019-09-11 21:56:04 -0700

    HomePage
    Homepage being put together

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import background from './images/SoundBridge.jpg';
+import logo from './OfficialsbLogo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="bg" src={background} alt="bg" ></img>
+      <div>
+        <img className="logo" src={logo} alt="logo"></img>
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
---
+++ src/components/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../components/login.css';
+
+function Login() {
+    return(
+        <div className=""></div>
+    );
+};
+
+export default Login;
