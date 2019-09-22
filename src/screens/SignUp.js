3cf5b78d893ecb9b6035320558c94d5e6e11d701
Anthony Martinez <Martinezanthony213@gmail.com>
2019-09-22 03:28:41 -0700

    Registration and Login Form feature
    Registration and Login form now fully funcational. Send post request to backend using axios and gives a JWT token when user is logged in.

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,44 +1,102 @@
 import React from 'react';
+import axios from 'axios';
 import './SignUp.css';
 import { FaFacebookF } from 'react-icons/fa';
 import { FaGooglePlusG } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
-import { Button, Input, Card, CardBody, Container } from 'reactstrap';
+import { Button, Input, Container, Form } from 'reactstrap';
 
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
+  }
+
+  onChangeName = event => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  onChangeEmail = event => {
+    this.setState({
+      email: event.target.value
+    });
+  }
+
+  onChangePassword = event => {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  onSubmit = async event => {
+    event.preventDefault()
+
+    const user = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    axios.post('http://localhost:4000/api/user/register', user)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    this.setState({
+      name: '',
+      email: '',
+      password: ''
+    })
+  }
+
+
+
   render() {
     return (
-        <Container className="justify-content-center">
-          <Card className="mx-auto">
-            <CardBody>
-              <div className="text-center">
-                <h3>SignUp</h3>
-              </div>
-              <Input type="text" name="firstName" id="firstName" placeholder="First Name"></Input>
-              <Input type="text" name="lastName" id="lastName" placeholder="Last Name"></Input>
-              <Input type="email" name="email" id="email" placeholder="Email"></Input>
-              <Input type="password" name="password" id="password" placeholder="Password"></Input>
-              <div className="text-center">
-                <Button id="formbtn" className="btn btn-md">Submit</Button>
-              </div>
-              <hr></hr>
-              <p id="option" className="d-flex justify-content-center mb-3 pt-2">Or Sign in with:</p>
-              <div className="row my-3 d-flex justify-content-around">
-                <Button className="rounded-pill">
-                  <FaFacebookF />
-                </Button>
-                <Button className="rounded-pill">
-                  <FaTwitter />
-                </Button>
-                <Button className="rounded-pill">
-                  <FaGooglePlusG />
-                </Button>
-              </div>
-            </CardBody>
-          </Card>
-        </Container>
+      <Container className="justify-content-center">
+        <Form className="mx-auto" onSubmit={this.onSubmit}>
+          <div className="cardbody">
+            <div className="text-center">
+              <h3>SignUp</h3>
+            </div>
+            <Input type="text" name="name" id="name" placeholder="Name" value={this.state.name} onChange={this.onChangeName} />
+            <Input type="email" name="email" id="email" placeholder="Email" value={this.state.email} onChange={this.onChangeEmail} />
+            <Input type="password" name="password" id="password" placeholder="Password" value={this.state.password} onChange={this.onChangePassword} />
+            <div className="text-center">
+              <Button id="formbtn" type="submit" className="btn btn-md">Submit</Button>
+            </div>
+            <hr></hr>
+            <p id="option" className="d-flex justify-content-center mb-3 pt-2">Or Sign in with:</p>
+            <div className="row my-3 d-flex justify-content-around">
+              <Button className="rounded-pill">
+                <FaFacebookF />
+              </Button>
+              <Button className="rounded-pill">
+                <FaTwitter />
+              </Button>
+              <Button className="rounded-pill">
+                <FaGooglePlusG />
+              </Button>
+            </div>
+          </div>
+        </Form>
+      </Container>
     );
   };
 };
-
 export default SignUp;
