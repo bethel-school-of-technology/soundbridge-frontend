0323a2ddce1f77471bca5f0962012902e9708477
Anthony Martinez <Martinezanthony213@gmail.com>
2019-09-26 16:27:47 -0700

    Fixed Icons
    Icons now displayed with animation and hero img now covers the background on mac.

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
-} from 'reactstrap';
+import { Button } from 'reactstrap';
 import './About.css';
 import './Contact.css';
 import { Animated } from "react-animated-css";
+import { FaFacebookF } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+
 
 const About = () => (
   <div>
@@ -55,7 +57,7 @@ const About = () => (
             <hr></hr>
           </div>
           <div className="about-text-content">
-            <p>Together, we aim to connect Listeners and Musicians in a tight-nit, online community.</p> 
+            <p>Together, we aim to connect Listeners and Musicians in a tight-nit, online community.</p>
             <p>As musicians ourselves, we want to make it easy to colaberate with other artists, find new listeners and even discover new venues and events!</p>
           </div>
 
@@ -79,59 +81,66 @@ const About = () => (
             <p>our Location<br></br>Our City, AA 222</p>
             <strong>Contact Info</strong>
             <p>(222) 222-2222<br></br>email@soundbridge.com</p>
-            <a href="#">
-              <i class="fab fa-facebook-square"></i>
-              <i class="fab fa-twitter-square"></i>
-              <i class="fab fa-instagram"></i>
-              <i class="fab fa-reddit-square"></i>
-              <i class="fab fa-linkedin"></i>
-            </a>
-
-          </div>
-
-          <div class="col-md-7">
-            <h3>Have a Question?</h3>
-
-            <form id="contact-form" method="post" action="contact.php">
-
-              <div class="messages"></div>
-              <div class="controls">
-
-                <div class="form-group">
-                  <input id="form_name" type="text" name="name" class="form-control"
-                    placeholder="Enter your name." required="required"></input>
-                </div>
-
-                <div class="form-group">
-                  <input id="form_email" type="email" name="email" class="form-control"
-                    placeholder="Enter your email." required="required"></input>
-                </div>
-
-                <div class="form-group">
-                  <textarea id="form_message" name="message" class="form-control"
-                    placeholder="Add your message." rows="4" required="required"></textarea>
-                </div>
-
-                <input type="submit" class="btn btn-outline-light btn-sm" value="Send message"></input>
-
+            <div id="icons" className="row">
+              <Button className=" fa rounded-pill">
+                <FaFacebookF />
+              </Button>
+              <Button className=" fa rounded-pill" >
+                <FaTwitter />
+              </Button>
+              <Button className=" fa rounded-pill">
+                <FaInstagram />
+              </Button>
+              <Button className=" fa rounded-pill">
+                <FaLinkedin />
+              </Button>
               </div>
 
-            </form>
+            </div>
 
-          </div>
+            <div class="col-md-7">
+              <h3>Have a Question?</h3>
 
-          <hr class="socket"></hr>
+              <form id="contact-form" method="post" action="contact.php">
+
+                <div class="messages"></div>
+                <div class="controls">
+
+                  <div class="form-group">
+                    <input id="form_name" type="text" name="name" class="form-control"
+                      placeholder="Enter your name." required="required"></input>
+                  </div>
+
+                  <div class="form-group">
+                    <input id="form_email" type="email" name="email" class="form-control"
+                      placeholder="Enter your email." required="required"></input>
+                  </div>
+
+                  <div class="form-group">
+                    <textarea id="form_message" name="message" class="form-control"
+                      placeholder="Add your message." rows="4" required="required"></textarea>
+                  </div>
+
+                  <input type="submit" class="btn btn-outline-light btn-sm" value="Send message"></input>
+
+                </div>
+
+              </form>
+
+            </div>
+
+            <hr class="socket"></hr>
 
 
-        </div> {/*} <!-- End Row --> */}
+          </div> {/*} <!-- End Row --> */}
       </footer> {/*} <!-- End Footer --> */}
 
 
 
-      {/* End Contact Section */}
+        {/* End Contact Section */}
     </div>
 
-  </div>
-);
-
+    </div>
+    );
+    
 export default About;
