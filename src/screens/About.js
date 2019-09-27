3cb4f79ea6b80d857057dd7ccf75ca3c7aa149b8
Anthony Martinez <Martinezanthony213@gmail.com>
2019-09-27 14:26:04 -0700

    Restored filed
    reverted file before overview merge

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,18 +1,19 @@
 import React from 'react';
-// import {
-//   Card, CardImg, CardText, CardBody,
-//   CardTitle, CardSubtitle, Button
-// } from 'reactstrap';
+import {Button} from 'reactstrap';
 import './About.css';
 import './Contact.css';
 import { Animated } from "react-animated-css";
+import { FaFacebookF } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 
 const About = () => (
   <div>
 
-    <div class="landing">
-      <div class="home-wrap">
-        <div class="home-inner">
+    <div className="landing">
+      <div className="home-wrap">
+        <div className="home-inner">
 
         </div>
       </div>
@@ -20,23 +21,23 @@ const About = () => (
 
 
 
-    <div class="caption center-block text-center">
-      <Animated animationIn="fadeInUp" animationOut="fadeOut" isVisible={true} animationInDelay="600">
+    <div className="caption center-block text-center">
+      <Animated animationIn="fadeInUp" animationOut="fadeOut" isVisible={true} animationInDelay={600}>
         <h3>We bridge the gap between</h3>
       </Animated>
-      <Animated animationIn="fadeInUp" animationOut="fadeOut" isVisible={true} animationInDelay="1500">
+      <Animated animationIn="fadeInUp" animationOut="fadeOut" isVisible={true} animationInDelay={1500}>
         <h3>The Artist and the Listener.</h3>
       </Animated>
-      <Animated animationIn="fadeInUp" animationOut="fadeOut" isVisible={true} animationInDelay="2000">
-        <a class="btn btn-outline-light btn-lg" href="#about">Learn More</a>
+      <Animated animationIn="fadeInUp" animationOut="fadeOut" isVisible={true} animationInDelay={2000}>
+        <a className="btn btn-outline-light btn-lg" href="#about">Learn More</a>
       </Animated>
     </div>
 
-    <Animated animationIn="fadeInRight" animationOut="fadeOut" isVisible={true} animationInDelay="2000">
-      <div class="narrow text-center">
-        <div class="col-12">
+    <Animated animationIn="fadeInRight" animationOut="fadeOut" isVisible={true} animationInDelay={2000}>
+      <div className="narrow text-center">
+        <div className="col-12">
           <h1>Who We Are</h1>
-          <p class="lead">SoundBridge is a social music platform founded by four passionate software developers in California.
+          <p className="lead">SoundBridge is a social music platform founded by four passionate software developers in California.
            </p>
         </div>
       </div>
@@ -66,12 +67,12 @@ const About = () => (
 
     {/* Start Contact Section */}
 
-    <div id="contact" class="offset">
+    <div id="contact" className="offset">
 
       <footer>
-        <div class="row">
+        <div className="row">
 
-          <div class="col-md-5 contact-info">
+          <div className="col-md-5 contact-info">
             <h1>Sound Bridge</h1>
             <p>We are the bridge between Artists and their listeners.
             </p>
@@ -79,40 +80,47 @@ const About = () => (
             <p>our Location<br></br>Our City, AA 222</p>
             <strong>Contact Info</strong>
             <p>(222) 222-2222<br></br>email@soundbridge.com</p>
-            <a href="/#">
-              <i class="fab fa-facebook-square"></i>
-              <i class="fab fa-twitter-square"></i>
-              <i class="fab fa-instagram"></i>
-              <i class="fab fa-reddit-square"></i>
-              <i class="fab fa-linkedin"></i>
-            </a>
+            <div id="icons" className="row">
+              <Button className=" fa rounded-pill">
+                <FaFacebookF />
+              </Button>
+              <Button className=" fa rounded-pill" >
+                <FaTwitter />
+              </Button>
+              <Button className=" fa rounded-pill">
+                <FaInstagram />
+              </Button>
+              <Button className=" fa rounded-pill">
+                <FaLinkedin />
+              </Button>
+              </div>
 
           </div>
 
-          <div class="col-md-7">
+          <div className="col-md-7">
             <h3>Have a Question?</h3>
 
             <form id="contact-form" method="post" action="contact.php">
 
-              <div class="messages"></div>
-              <div class="controls">
+              <div className="messages"></div>
+              <div className="controls">
 
-                <div class="form-group">
-                  <input id="form_name" type="text" name="name" class="form-control"
+                <div className="form-group">
+                  <input id="form_name" type="text" name="name" className="form-control"
                     placeholder="Enter your name." required="required"></input>
                 </div>
 
-                <div class="form-group">
-                  <input id="form_email" type="email" name="email" class="form-control"
+                <div className="form-group">
+                  <input id="form_email" type="email" name="email" className="form-control"
                     placeholder="Enter your email." required="required"></input>
                 </div>
 
-                <div class="form-group">
-                  <textarea id="form_message" name="message" class="form-control"
+                <div className="form-group">
+                  <textarea id="form_message" name="message" className="form-control"
                     placeholder="Add your message." rows="4" required="required"></textarea>
                 </div>
 
-                <input type="submit" class="btn btn-outline-light btn-sm" value="Send message"></input>
+                <input type="submit" className="btn btn-outline-light btn-sm" value="Send message"></input>
 
               </div>
 
@@ -120,7 +128,7 @@ const About = () => (
 
           </div>
 
-          <hr class="socket"></hr>
+          <hr className="socket"></hr>
 
 
         </div> {/*} <!-- End Row --> */}
