453b620ef98405d7d1ebc94dd4f96a8c0c52a6b2
CameronP <nokaperrin@gmail.com>
2019-10-01 12:55:31 -0700

    .

---
+++ src/components/Playlist.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { SpotifyApiMethods} from '../../services/spotifyApiMethods';
+
+const listStyle = {
+    'marginTop': '1em',
+    // 'background': `url(${playlist.images[0].url})`,
+    'backgroundRepeat': 'no-repeat',
+    'height': '250px',
+    'width': '250px',
+    'display': 'flex',
+    'flexDirection': 'column',
+    'justifyContent': 'flex-start',
+    'alignItems': 'center',
+    'color': '#fff'
+}
+
+class Playlists extends Component {
+
+    state = {
+        tracks: {}
+    }
+
+    async componentDidMount() {
+        const tracks = await SpotifyApiMethods.getPlaylistTracks(this.props.playlists.tracks.href, this.props.accessToken);
+        this.setState({ tracks })
+    }
+
+    render() {
+        console.log(this.props.playlists);
+        const tracks = this.state.tracks.items;
+        if (tracks === undefined) {
+            return <h1>loading</h1>
+        }
+        console.log('tracks: ', tracks);
+        return (
+            <div style={listStyle}>
+                <h4>Title: {this.props.playlists.name}</h4>
+                <p>Tracks: </p>
+                {
+                    tracks.map((tracks, i) => {
+                        return <p key={i} >{tracks.track.name} <em>by</em> {tracks.track.artists[0].name}</p>
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default Playlists;
