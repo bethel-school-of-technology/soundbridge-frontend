6f21da71a05ebb6e7c7b9c5f35abc9ebab5b3588
CameronP <nokaperrin@gmail.com>
2019-10-03 12:11:28 -0700

    fixed bugs

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Col, Input, FormGroup, Label } from 'reactstrap';
 import './Home.css';
 
 const Home = () => (
   <div>
+
+    <FormGroup row>
+      <Col sm={10}>
+        <Input type="textarea" name="text" id="What's on your mind?" />
+      </Col>
+    </FormGroup>
 
     <div className="home-page-content page-header section-dark" style={{
       backgroundImage:
