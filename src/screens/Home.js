b6a7bd4a86d23f3a0774678c70035cb8791f0b81
CameronP <nokaperrin@gmail.com>
2019-10-03 12:26:45 -0700

    .

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,12 +5,6 @@ import './Home.css';
 const Home = () => (
   <div>
 
-    <FormGroup row>
-      <Col sm={10}>
-        <Input type="textarea" name="text" id="What's on your mind?" />
-      </Col>
-    </FormGroup>
-
     <div className="home-page-content page-header section-dark" style={{
       backgroundImage:
         "url(" + require("../assets/images/SoundBridge.jpg") + ")"
