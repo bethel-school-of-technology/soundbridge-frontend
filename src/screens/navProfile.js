f9ee5fea39d4f72d2bab4a04c87896b4b5075a8b
Anthony Pereira <anthonyp@ichop.org>
2019-10-06 16:44:19 -0700

    Changed backend calls to go to heroku server

--- src/screens/navProfile.js
+++ src/screens/navProfile.js
@@ -25,7 +25,7 @@ export default class Profile extends React.Component {
 
   componentDidMount() {
     const params = this.props.location.state.info;
-    axios.post('http://localhost:4000/has-spotify/' + params.spotifyRefreshToken)
+    axios.post('https://soundbridge.herokuapp.com/has-spotify/' + params.spotifyRefreshToken)
       .then(res => {
         this.setState({
           accessToken: res.data,
