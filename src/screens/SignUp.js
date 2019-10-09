64476c37830b28afb8e15b1be050615441ca7202
Anthony Pereira <anthonyp@ichop.org>
2019-10-09 16:32:21 -0700

    Little change

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -5,7 +5,6 @@ import { FaFacebookF } from 'react-icons/fa';
 import { FaGooglePlusG } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
 import { Button, Card, CardBody, Container } from 'reactstrap';
-import { Redirect } from 'react-router-dom';
 
 class SignUp extends React.Component {
 
