f8ab9cd4c90f9e595116289e22ddfd1804408bf2
Mike <mikemolner@gmail.com>
2019-10-10 14:23:54 -0700

    trying to add card

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,48 +1,60 @@
 import React from 'react';
-import { Container, Label, Col, Row, Input, FormGroup, Button, Form } from 'reactstrap';
+import {
+  Container, Label, Col, Row, Input, FormGroup, Button, Form,
+  Card, CardHeader, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle, CardBlock, Textarea
+} from 'reactstrap';
 import './Home.css';
 
 const Home = () => (
   <div>
-     
 
-    <div className="home-page-content page-header section-dark" style={{
+
+    {/* <div className="home-page-content page-header section-dark" style={{
       backgroundImage:
         "url(" + require("../assets/images/SoundBridge.jpg") + ")"
-    }} >
-  
-     <Container className="App">
-       
+    }} > */}
+
+      {/* <Container className="App">
         <Form className="form">
-         
-            <FormGroup>
-           
+          <FormGroup>
             <Label for="Text" className="h2"><h2>Create Post</h2></Label>
-              
-            <Input type="textarea" className="text" id="Post" 
-                placeholder="Share Your Thoughts" 
-              />
-              
-              
-            </FormGroup>
-            
-          <Button className="btn">Post</Button> <Button className="btn2">Add Photo/Video</Button> 
-          
-        </Form>
-      </Container>
+            <Input type="textarea" className="text" id="Post"
+              placeholder="Share Your Thoughts"
+            /> */}
+
+            <div>
+              <Card style={{ width: '18rem' }}>
+                <CardBody className=".card">
+                  <CardTitle>Card Title</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted">Card Subtitle</CardSubtitle>
+                  <CardText >
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </CardText>
+                    <FormGroup>
+                  <CardLink href="#" >Card Link</CardLink><br></br>
+                  <CardLink href="#">Another Link</CardLink>
+                  </FormGroup>
+                </CardBody>
+              </Card>
+
     
+            </div>
 
+          {/* </FormGroup> */}
+          {/* <Button className="btn">Post</Button> <Button className="btn2">Add Photo/Video</Button> */}
+        {/* </Form>
 
-      <div className="filter">
-      
+      </Container> */}
+   <div className="filter"> 
         <div className="content-center"></div>
-      </div>
 
-    </div>
-   
+      </div> 
 
-  </div>
 
+     </div>
+  // </div>
 );
 
 export default Home;
